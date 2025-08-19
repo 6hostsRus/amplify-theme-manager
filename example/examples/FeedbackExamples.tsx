f587c3472651a8fdac3dcd23e834e81f2e38bf58
6hostsRus <175490611+6hostsRus@users.noreply.github.com>
2025-08-18 19:41:13 -0500

    feat: expand kitchen sink examples

--- example/examples/FeedbackExamples.tsx
+++ example/examples/FeedbackExamples.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Flex, Alert, Button } from '@aws-amplify/ui-react';
+import { Flex, Alert, Button, Loader, Message } from '@aws-amplify/ui-react';
 
 export const FeedbackExamples: React.FC = () => {
      const [visible, setVisible] = useState(true);
@@ -10,24 +10,33 @@ export const FeedbackExamples: React.FC = () => {
                </Button>
                {visible && (
                     <Flex direction="column" gap="0.75rem">
-                         <Alert
-                              variation="info"
-                              isDismissible={true}
-                              heading="Info Alert"
-                         >
-                              General informational message.
-                         </Alert>
-                         <Alert variation="success" heading="Success Alert">
-                              Action completed successfully.
-                         </Alert>
-                         <Alert variation="warning" heading="Warning Alert">
-                              Something may require attention.
-                         </Alert>
-                         <Alert variation="error" heading="Error Alert">
-                              An error occurred.
-                         </Alert>
-                    </Flex>
+                        <Alert
+                             variation="info"
+                             isDismissible={true}
+                             heading="Info Alert"
+                        >
+                             General informational message.
+                        </Alert>
+                        <Alert variation="success" heading="Success Alert">
+                             Action completed successfully.
+                        </Alert>
+                        <Alert variation="warning" heading="Warning Alert">
+                             Something may require attention.
+                        </Alert>
+                        <Alert variation="error" heading="Error Alert">
+                             An error occurred.
+                        </Alert>
+                   </Flex>
                )}
+               <Loader size="large" variation="linear" />
+               <Message
+                    variation="outlined"
+                    colorTheme="info"
+                    heading="Info Message"
+                    isDismissible
+               >
+                    Additional context for the user.
+               </Message>
           </Flex>
      );
 };
